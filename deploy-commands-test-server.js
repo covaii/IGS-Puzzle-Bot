2d0e8fd9237bdda0548af2666f2383981158bdfe
Stone Widder <skwidder@gmail.com>
2024-12-12 15:27:16 -0600

    remove ping command, split out deploy commands

--- deploy-commands-test-server.js
+++ deploy-commands-test-server.js
@@ -34,7 +34,7 @@ const rest = new REST().setToken(discord_token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationCommands(discord_clientId),
+			Routes.applicationGuildCommands(discord_clientId, discord_guildId),
 			{ body: commands },
 		);
 
